f61bc9f95ffb4e38e5ad350b2958178db63f6698
imksav <58361148+imksav@users.noreply.github.com>
2019-12-06 16:10:21 +0545

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blue),
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+        new Image(
+          image: new AssetImage("assets/me.jpg"),
+          fit: BoxFit.cover,
+          colorBlendMode: BlendMode.hardLight,
+          color: Colors.black87,
+        ),
+        new Theme(
+          data: new ThemeData(
+
+       
+              brightness: Brightness.dark,
+              inputDecorationTheme: new InputDecorationTheme(
+                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+                labelStyle:
+                    new TextStyle(
+                      color: Colors.red,
+                      fontSize: 35.0),
+              )),
+          isMaterialAppTheme: true,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 80.0,
+              ),
+              new Container(
+                padding: const EdgeInsets.all(60.0),
+                child: new Form(
+                  autovalidate: true,
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                            labelText: "Enter Email",
+                            fillColor: Colors.green),
+                          keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                      ),
+                      new MaterialButton(
+                        height: 50.0,
+                        minWidth: 150.0,
+                        color: Colors.green,
+                        splashColor: Colors.red,
+                        textColor: Colors.black,
+                        child: new Icon(FontAwesomeIcons.signInAlt),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
